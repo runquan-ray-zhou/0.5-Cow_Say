7c29ca1893b8fa402b02859ed84ac5aa68ad6184
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-10-24 21:55:37 -0400

    Update title and add favicon

--- src/components/CowSay.jsx
+++ src/components/CowSay.jsx
@@ -1,30 +1,107 @@
-import { say, PTERODACTYL, OCTOPUS, WHALE, TURTLE, TURKEY, STEGOSAURUS, SQUIRREL, SQUID, SPIDERCOW, SNOOPY, SHEEP, SEAHORSE, OWL, NYAN, MOOSE, MULE, MEOW, LOBSTER, LAMB, LAMB2, KITTY, KITTEN, JELLYFISH, IWASHI, HYPNO, HOMER, HIYOKO, HIYA, HEDGEHOG, ELEPHANT2, DRAGON, DOLPHIN, CHARIZARDVICE, CAT2, CAT, BEES, BEARFACE, ARMADILLO} from 'cowsay';
-import { useState } from 'react';
+import {
+  say,
+  PTERODACTYL,
+  OCTOPUS,
+  WHALE,
+  TURTLE,
+  TURKEY,
+  STEGOSAURUS,
+  SQUIRREL,
+  SQUID,
+  SPIDERCOW,
+  SNOOPY,
+  SHEEP,
+  SEAHORSE,
+  OWL,
+  NYAN,
+  MOOSE,
+  MULE,
+  MEOW,
+  LOBSTER,
+  LAMB,
+  LAMB2,
+  KITTY,
+  KITTEN,
+  JELLYFISH,
+  IWASHI,
+  HYPNO,
+  HOMER,
+  HIYOKO,
+  HIYA,
+  HEDGEHOG,
+  ELEPHANT2,
+  DRAGON,
+  DOLPHIN,
+  CHARIZARDVICE,
+  CAT2,
+  CAT,
+  BEES,
+  BEARFACE,
+  ARMADILLO,
+} from "cowsay";
+import { useState } from "react";
 
-const array = [PTERODACTYL, OCTOPUS, WHALE, TURTLE, TURKEY, STEGOSAURUS, SQUIRREL, SQUID, SPIDERCOW, SNOOPY, SHEEP, SEAHORSE, OWL, NYAN, MOOSE, MULE, MEOW, LOBSTER, LAMB, LAMB2, KITTY, KITTEN, JELLYFISH, IWASHI, HYPNO, HOMER, HIYOKO, HIYA, HEDGEHOG, ELEPHANT2, DRAGON, DOLPHIN, CHARIZARDVICE, CAT2, CAT, BEES, BEARFACE, ARMADILLO]
+const array = [
+  PTERODACTYL,
+  OCTOPUS,
+  WHALE,
+  TURTLE,
+  TURKEY,
+  STEGOSAURUS,
+  SQUIRREL,
+  SQUID,
+  SPIDERCOW,
+  SNOOPY,
+  SHEEP,
+  SEAHORSE,
+  OWL,
+  NYAN,
+  MOOSE,
+  MULE,
+  MEOW,
+  LOBSTER,
+  LAMB,
+  LAMB2,
+  KITTY,
+  KITTEN,
+  JELLYFISH,
+  IWASHI,
+  HYPNO,
+  HOMER,
+  HIYOKO,
+  HIYA,
+  HEDGEHOG,
+  ELEPHANT2,
+  DRAGON,
+  DOLPHIN,
+  CHARIZARDVICE,
+  CAT2,
+  CAT,
+  BEES,
+  BEARFACE,
+  ARMADILLO,
+];
 
 function CowGreeter() {
+  const [character, setCharacter] = useState(null);
 
-const [character, setCharacter] = useState(null)
+  function handleCharacterChange() {
+    setCharacter(array[Math.round(Math.random() * array.length)]);
+  }
 
-function handleCharacterChange() {
-  setCharacter(array[Math.round(Math.random() * array.length)])
-}
-  
   const char = say({
-    text: 'Find your spirit animal!',
-    cow: character
-  }).replaceAll("\n", "<br/>").replaceAll(" ", "&nbsp")
+    text: "Find your spirit animal!",
+    cow: character,
+  })
+    .replaceAll("\n", "<br/>")
+    .replaceAll(" ", "&nbsp");
 
-    return (
-
-      <div>
-        <input onClick={handleCharacterChange} type="button" value="Click Me!"/>
-        <div dangerouslySetInnerHTML={{__html: `<div>${char}</div>` }} />
-      </div>
-
-    )
-
+  return (
+    <div className="component-container">
+      <input onClick={handleCharacterChange} type="button" value="Click Me!" />
+      <div dangerouslySetInnerHTML={{ __html: `<div>${char}</div>` }} />
+    </div>
+  );
 }
 
-export default CowGreeter
+export default CowGreeter;
